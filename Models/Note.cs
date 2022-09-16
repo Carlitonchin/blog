cd9e2c6b2e75134ee749e95085f50ec90715ca40
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-16 15:41:26 -0300

    not regular expression for title and body for notes

--- Models/Note.cs
+++ Models/Note.cs
@@ -8,13 +8,11 @@ public class Note{
     public int Id {get; set;}
 
     [Required(ErrorMessage ="Inserte un título")]
-     [RegularExpression(@"\s*[^[\s]]*\s*", ErrorMessage ="Inserte un título")] //ony blank spaces not allowed
      [StringLength(100, ErrorMessage = "El título debe tener máximo 100 caracteres")]
      [Display(Name = "Título")]
     public string? Title {get; set;}
 
     [Required(ErrorMessage ="Escriba algo en el cuerpo")]
-     [RegularExpression(@"\s*[^[\s]]*\s*", ErrorMessage ="Escriba algo en el cuerpo")] //ony blank spaces not allowed
      [StringLength(250000, ErrorMessage = "El cuerpo debe tener máximo 250 mil caracteres")]
      [Display(Name = "Título")]
     public string? Body {get; set;}
