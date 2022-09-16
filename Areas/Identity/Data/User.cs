0d814675f98bcbc36c927447b4b33237d1480a3a
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-16 12:53:27 -0300

    add Note model

--- Areas/Identity/Data/User.cs
+++ Areas/Identity/Data/User.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blog.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace blog.Areas.Identity.Data;
@@ -9,5 +10,6 @@ namespace blog.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the User class
 public class User : IdentityUser
 {
+    public virtual ICollection<Note>? UserNotes {get; set;}
 }
 
