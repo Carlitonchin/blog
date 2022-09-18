baf5ce9cd725d36d0fe3622e515a5c72d2f7c8fa
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-18 20:59:42 -0300

    fix filter bug, filter title and body simultaneus

--- wwwroot/js/filter.js
+++ wwwroot/js/filter.js
@@ -1,6 +1,9 @@
 var titles = document.getElementsByClassName("title-note");
 var bodies = document.getElementsByClassName("body-note");
 
+var title_filter = document.getElementById("title_filter");
+var body_filter = document.getElementById("body_filter");
+
 function filter_title(text){
     for(let i = 0; i < titles.length; i++)
     {
@@ -24,8 +27,25 @@ function filter_body(text){
     }
 }
 
-var title_filter = document.getElementById("title_filter");
-var body_filter = document.getElementById("body_filter");
+function filter(){
+    for(let i = 0; i < titles.length; i++)
+    {
+        let title = titles[i]
+        let body = bodies[i]
 
-title_filter.addEventListener("input", e=>filter_title(e.target.value))
-body_filter.addEventListener("input", e=>filter_body(e.target.value))
+        let text_title = title_filter.value
+        let text_body = body_filter.value
+
+        let parent = title.parentNode.parentNode.parentNode;
+
+        let ocurr_title = title.innerHTML.toLowerCase().includes(text_title.toLowerCase())
+        let ocurr_body = body.innerHTML.toLowerCase().includes(text_body.toLowerCase())
+
+        parent.style.display = ocurr_title && ocurr_body ? "block":"none";
+    }
+
+    
+}
+
+title_filter.addEventListener("input", filter)
+body_filter.addEventListener("input", filter)
