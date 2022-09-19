ac85c461280281f4dbb9952e90c72cf37716ab3e
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-18 21:02:31 -0300

    remove unnecesary filters functions

--- wwwroot/js/filter.js
+++ wwwroot/js/filter.js
@@ -4,29 +4,6 @@ var bodies = document.getElementsByClassName("body-note");
 var title_filter = document.getElementById("title_filter");
 var body_filter = document.getElementById("body_filter");
 
-function filter_title(text){
-    for(let i = 0; i < titles.length; i++)
-    {
-        let title = titles[i]
-        let parent = title.parentNode.parentNode.parentNode;
-        let ocurr = title.innerHTML.toLowerCase().includes(text.toLowerCase())
-
-        parent.style.display = ocurr ? "block":"none";
-    }
-    
-}
-
-function filter_body(text){
-    for(let i = 0; i < bodies.length; i++)
-    {
-        let body = bodies[i]
-        let parent = body.parentNode.parentNode;
-        let ocurr = body.innerHTML.toLowerCase().includes(text.toLowerCase())
-
-        parent.style.display = ocurr ? "block":"none";
-    }
-}
-
 function filter(){
     for(let i = 0; i < titles.length; i++)
     {
@@ -43,8 +20,6 @@ function filter(){
 
         parent.style.display = ocurr_title && ocurr_body ? "block":"none";
     }
-
-    
 }
 
 title_filter.addEventListener("input", filter)
