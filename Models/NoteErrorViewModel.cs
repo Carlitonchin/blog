f911dbf7ad9fbde2284bc6786801009d020ba6f2
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-24 15:13:23 -0300

    add some function to create errors

--- Models/NoteErrorViewModel.cs
+++ Models/NoteErrorViewModel.cs
@@ -10,4 +10,35 @@ public class NoteErrorViewModel{
         this.Message = message;
         this.Action = action;
     }
+
+    private static NoteErrorViewModel NewNoteErrorViewModel(int status, string action, int note_id){
+        string message = "";
+
+        if(status == CodeError.NotFound)
+            message = "La nota con id = '" + note_id + "' no existe";
+        else if(status == CodeError.NotFound)
+            message = "Usted no es el autor de la nota con id ='" + note_id + "'";
+        else
+            message = "Ocurrió un error inesperado, por favor contacte con el administrador";
+
+        return new NoteErrorViewModel(status, message, action);
+    }
+
+    public static NoteErrorViewModel Internal(string action, int note_id){
+        return NewNoteErrorViewModel(CodeError.Internal, action, note_id);
+    }
+
+    public static NoteErrorViewModel NotFound(string action, int note_id){
+        return NewNoteErrorViewModel(CodeError.NotFound, action, note_id);
+    }
+
+    public static NoteErrorViewModel NotAuthorized(string action, int note_id){
+        return NewNoteErrorViewModel(CodeError.NotAuthorized, action, note_id);
+    }
+}
+
+ static class CodeError{
+    public const int NotFound = 404;
+    public const int NotAuthorized = 401;
+    public const int Internal = 500;
 }
